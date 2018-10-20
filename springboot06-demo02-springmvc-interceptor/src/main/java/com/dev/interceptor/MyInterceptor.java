76acf179dfadb9032c409b563188cbfe07c53bab
xiaotian.huang <hsiaotien@163.com>
2018-10-20 11:39:38 +0800

    添加日志支持

--- springboot06-demo02-springmvc-interceptor/src/main/java/com/dev/interceptor/MyInterceptor.java
+++ springboot06-demo02-springmvc-interceptor/src/main/java/com/dev/interceptor/MyInterceptor.java
@@ -1,5 +1,7 @@
 package com.dev.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,19 +10,24 @@ import javax.servlet.http.HttpServletResponse;
 
 public class MyInterceptor implements HandlerInterceptor {
 
+	private Logger logger = LoggerFactory.getLogger(MyInterceptor.class);
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		logger.debug("preHandle is running");
 		System.out.println("拦截器的前置方法执行了");
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		logger.debug("postHandle is running");
 		System.out.println("拦截器的后置方法执行了");
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		logger.debug("afterCompletion is running");
 		System.out.println("拦截器的最终方法执行了");
 	}
 }
